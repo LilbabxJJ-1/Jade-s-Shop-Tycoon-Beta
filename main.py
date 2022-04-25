314e481f599f0adca6396a300c2a7ac9c4901d3e
JJ <99676249+LilbabxJJ-1@users.noreply.github.com>
2022-04-25 19:03:14 +0000

    update

--- main.py
+++ main.py
@@ -7,71 +7,109 @@ from termcolor import colored
 import options
 import var
 
- 
-def inv_tutorial():
-  replit.clear()
-  print(f"{var.username}'s Inventory")
-  print("-------------------------------")
-  for i in var.items:
-    print(f"{i} - {var.items[i]}")
-  var.mycol.insert_one(var.items)
-  time.sleep(1.5)
-  print("This is your inventory! Small but can grow quickly with a bit of help!")
-  time.sleep(1.5)
-  print("If you ever need to see the description of a item, you can always ")
-  
-  
-def tutorial():
-  replit.clear()
-  go_to = None
-  print(f"{var.username}'s Shopˏˋ°•*⁀➷")
-  print("--------------------------")
-  print("[1]Inventory\n[2]Orders\n[3]Buy\n[4]Sell")
-  time.sleep(2.5)
-  print(colored(f"This is your own shop! You can check your inventory..", "cyan"))
-  time.sleep(2.5)
-  print(colored("Buy things!", "green"))
-  time.sleep(2.5)
-  print(colored("Check incoming orders! And so much more! (Coming soon..)", "red"))
-  time.sleep(2.5)
-  print("We're gonna do the tutorial first! Please enter option 1 to start with inventory!")
-  while go_to == None:
-    try:
-      go_to = int(input(''))
-      if go_to == 1:
-        inv_tutorial()
-      elif go_to == 2:
-        quit()
-      else:
-        print("Please try that again! Put the number 1")
-        go_to = None
-    except ValueError:
-      print("Please try that again! Put the number 1")
 
+def inv_tutorial():
+    replit.clear()
+    print(f"{var.username}'s Inventory")
+    print("-------------------------------")
+    var.items["Item_holder"] = var.username
+    for i in var.items:
+        if i == "Item_holder":
+            pass
+        else:
+            print(f"{i} - {var.items[i]}")
+    var.mycol.insert_one(var.items)
+    time.sleep(1.5)
+    print(
+        "This is your inventory! Small but can grow quickly with a bit of help!"
+    )
+    time.sleep(1.5)
+    print("If you ever need to see the description of a item, you can always just type the item name. Try it now!")
+  
+    description = None
+    while description is None:
+        description = input("Item name: ")
+        if description.lower() in var.item_descriptions:
+          try:
+            print(var.item_descriptions[description.lower()])
+            break
+          except KeyError:
+            print("That item does not have a description yet!")
+        else:
+            print("\nItem does not exist! Try again!")
+            description = None
+
+
+def tutorial():
+    replit.clear()
+    go_to = None
+    print(f"{var.username}'s Shopˏˋ°•*⁀➷")
+    print("--------------------------")
+    print("[1]Inventory\n[2]Orders\n[3]Buy\n[4]Sell")
+    time.sleep(2.5)
+    print(
+        colored(f"This is your own shop! You can check your inventory..",
+                "cyan"))
+    time.sleep(2.5)
+    print(colored("Buy things!", "green"))
+    time.sleep(2.5)
+    print(
+        colored("Check incoming orders! And so much more! (Coming soon..)",
+                "red"))
+    time.sleep(2.5)
+    print(
+        "We're gonna do the tutorial first! Please enter option 1 to start with inventory!"
+    )
+    while go_to == None:
+        try:
+            go_to = int(input(''))
+            if go_to == 1:
+                inv_tutorial()
+            elif go_to == 2:
+                quit()
+            else:
+                print("Please try that again! Put the number 1")
+                go_to = None
+        except ValueError:
+            print("Please try that again! Put the number 1")
+
+#-----------------------------------------------------------
+#Tutorial above
 
 def intro():
-  replit.clear()
-  italics = '\033[3m'
-  end = '\033[0m'
-  print(colored(f"Hey! It's you!! I've been waiting a long bit for you to arrive, {var.username}, right?", "cyan"))
-  time.sleep(4)
-  print(colored(f"I'm Jade, the very very very amazing shop owner! I'm the best at literally everything I do! I-.. {italics}Jade notices her ranmbling quickly fixes her posture.{end}", "cyan"))
-  print(colored("Sorry!! Anyways, you're new around, yeah? I'm excited to see a new shop owner in the making! Check out everything and come back to me when you're ready!", "cyan"))
-  time.sleep(7)
-  tutorial()
+    replit.clear()
+    italics = '\033[3m'
+    end = '\033[0m'
+    print(
+        colored(
+            f"Hey! It's you!! I've been waiting a long bit for you to arrive, {var.username}, right?",
+            "cyan"))
+    time.sleep(4)
+    print(
+        colored(
+            f"I'm Jade, the very very very amazing shop owner! I'm the best at literally everything I do! I-.. {italics}Jade notices her ranmbling quickly fixes her posture.{end}",
+            "cyan"))
+    print(
+        colored(
+            "Sorry!! Anyways, you're new around, yeah? I'm excited to see a new shop owner in the making! Check out everything and come back to me when you're ready!",
+            "cyan"))
+    time.sleep(7)
+    tutorial()
+
 
 def guest():
-  global username
-  global logins
-  var.username = "Guest"
-  var.logins = "Guests"
-  replit.clear()
-  red = '\033[91m'
-  end = '\033[0m'
-  print(f"You are now playing a guest and your data will {red}NOT{end} be saved if you refresh or leave this tab")
-  print("To log in to your account or make one, please restart the program")
-  menu()
-
+    global username
+    global logins
+    var.username = "Guest"
+    var.logins = "Guests"
+    replit.clear()
+    red = '\033[91m'
+    end = '\033[0m'
+    print(
+        f"You are now playing a guest and your data will {red}NOT{end} be saved if you refresh or leave this tab"
+    )
+    print("To log in to your account or make one, please restart the program")
+    menu()
 
 
 def login():
@@ -85,29 +123,38 @@ def login():
         var.username = input("Username: ")
         password = input("Password: ")
         try:
-            if var.mycol.find_one({"username": var.username}) == None:  # Searches database for user log
-                print(colored("\nAccount not found! (case sensitive)\n", "red"))
+            if var.mycol.find_one(
+                {"username":
+                 var.username}) == None:  # Searches database for user log
+                print(colored("\nAccount not found! (case sensitive)\n",
+                              "red"))
                 var.logins = "no"
                 tries += 1
                 if tries % 3 == 0:
-                    print(colored(
-                        "\nTip... You can sign up by restarting and selecting option 1", "green")
-                    )
+                    print(
+                        colored(
+                            "\nTip... You can sign up by restarting and selecting option 1",
+                            "green"))
             else:  # Verifies the password with a specific user
                 result = var.mycol.find({"username": var.username})
                 for i in result:
                     if i["password"] != password:
-                        print(colored("Incorrect Password! (case sensitive)","red"))
+                        print(
+                            colored("Incorrect Password! (case sensitive)",
+                                    "red"))
                         var.logins = "no"
                     else:
-                       var.logins = "yes"
+                        var.logins = "yes"
         except KeyError:
             pass
 
     result = var.mycol.find({"username": var.username})
     for i in result:
         if i["password"] == password:
-            print(colored(f"Welcome back {var.username}! Stats have been restored!", "white"))
+            print(
+                colored(
+                    f"Welcome back {var.username}! Stats have been restored!",
+                    "white"))
             time.sleep(2)
             replit.clear()
             var.logins = "No"
@@ -119,7 +166,7 @@ def signup():
     global password
     global logins
     var.username = "."
-    print(colored("\nUser SignUp","green"))
+    print(colored("\nUser SignUp", "green"))
     while var.username != "":
         var.username = input("Username: ")
         try:
@@ -131,7 +178,11 @@ def signup():
         except KeyError:
             pass
     var.password = input("Password: ")
-    userlog = {"username": var.username, "password": var.password, "Tutorial_Passed": var.tutorial_passed}
+    userlog = {
+        "username": var.username,
+        "password": var.password,
+        "Tutorial_Passed": var.tutorial_passed
+    }
     var.mycol.insert_one(userlog)
     replit.clear()
     logins = "No"
@@ -150,7 +201,9 @@ def signup():
 
 
 def user():
-    while (log := input("[1]New user?\n[2]Login\n[3]Play as Guest\n\nUse the numbers\n")) not in ["1", "2","3"]:
+    while (log := input(
+            "[1]New user? <-- (Recommended for new players)\n[2]Login\n[3]Play as Guest\n\nUse the numbers\n")
+           ) not in ["1", "2", "3"]:
         print("I don't know that option, please try again\n")
     if log == "1":
         signup()
@@ -161,26 +214,29 @@ def user():
 
 
 def menu():
-   print(colored(f"Welcome to Jade's Shop Tycoon", "magenta"))
-   if var.logins == False:
-     print("Not Logged In..")
-     user()
-   else:
-     print(f"Player {var.username}")
-     print("-------------------------------")
-     print("[1]Play \n[2]Stats \n[3]Exit/Log Out")
-     choice = int(input(""))
-     if choice == 1:
-       if var.username == "Guest":
-         intro()
-       else:  
-         result = var.mycol.find({"username": var.username})
-         for i in result:
-          if i["Tutorial_Passed"] == "False":
-            intro()
-     elif choice == 2:
-       status()
-     else:
-       quit()
- 
+    print(colored(f"Welcome to Jade's Shop Tycoon", "magenta"))
+    if var.logins == False:
+        print("Not Logged In..")
+        user()
+    else:
+        print(f"Player {var.username}")
+        print("-------------------------------")
+        print("[1]Play \n[2]Stats \n[3]Exit/Log Out")
+        choice = int(input(""))
+        if choice == 1:
+            if var.username == "Guest":
+                intro()
+            elif var.username != "Guest":
+                result = var.mycol.find({"username": var.username})
+                for i in result:
+                    if i["Tutorial_Passed"] == "False":
+                        intro()
+            else:
+              pass
+        elif choice == 2:
+            status()
+        else:
+            quit()
+
+
 menu()
--- var.py
+++ var.py
@@ -10,5 +10,11 @@ username = "Unknown"
 password = None
 logins = False
 tutorial_passed = False
+item_descriptions = {"coffee": 
+                     "Coffe: A cheap bevarage that anyone can come in to buy as a daily drink.",
+                     "bagel":
+                     "Bagel: Doughnut-shaped yeast-leavened roll that can be covered in many toppings or eaten alone"
+                    }
+
 
 items = {"Item_holder": username, "Coffee" : 1, "Bagel": 1, "Muffin": 1, "Chocolate": 0}
